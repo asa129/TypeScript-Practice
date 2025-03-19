284dc20314a5f2990fbf5811c26c4a489aee03cd
asa129 <asa.jad1290@gmail.com>
2025-03-19 22:23:22 +0900

    レスポンスのみとるように修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import './App.css'
 
 function App() {
   const onClickGetJson = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => console.log(response))
   }
 
   return (
